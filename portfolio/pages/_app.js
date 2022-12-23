d8e326c713dd9a2578f3cc3a5d7200366845242e
osawa-koki <github@osawa.dev>
2022-12-24 02:15:49 +0900

    スキルページ雛形。

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -5,6 +5,7 @@ import '../styles/index.scss';
 import '../styles/about.scss';
 import '../styles/portfolio.scss';
 import '../styles/resume.scss';
+import '../styles/skill.scss';
 import '../styles/faq.scss';
 
 export default function MyApp({ Component, pageProps }) {
---
+++ portfolio/pages/skill.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const SkillPage = () => (
+  <Layout title="osawa's skills">
+    <div id='Skill'>
+    </div>
+  </Layout>
+)
+
+export default SkillPage;
