c6842953d930ec8f808f040986a978d79b7a016f
osawa-koki <github@osawa.dev>
2022-12-23 22:02:01 +0900

    ポートフォリオサイト。

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -2,6 +2,7 @@
 import '../styles/Layout.scss';
 import '../styles/index.scss';
 import '../styles/about.scss';
+import '../styles/portfolio.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
