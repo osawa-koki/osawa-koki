1d8d2346b84776105518ce8e83ef10bcb1f68cef
osawa-koki <github@osawa.dev>
2022-12-23 20:47:35 +0900

    Aboutページ。

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,6 +1,7 @@
 
 import '../styles/Layout.scss';
 import '../styles/index.scss';
+import '../styles/about.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
