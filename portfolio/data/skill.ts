9114b1600d2570d580014e9d06108ad1c574ef95
osawa-koki <github@osawa.dev>
2022-12-24 15:28:07 +0900

    フォント変えた。

---
+++ portfolio/data/skill.ts
@@ -0,0 +1,142 @@
+
+type SkillCategory =
+  | 'Frontend'
+  | 'Backend'
+  | 'Database'
+  | 'OS'
+  | 'Cloud'
+
+type ChartData = {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    borderWidth: number;
+    borderColor: string;
+    backgroundColor: string;
+  }[];
+};
+
+type DataFormat = {
+  name: string;
+  hours: number;
+  category: SkillCategory;
+};
+
+const _skill: DataFormat[] = [
+  // Frontend
+  { name: "HTML&CSS", hours: 200, category: "Frontend" },
+  { name: "JavaScript", hours: 80, category: "Frontend" },
+  { name: "TypeScript", hours: 120, category: "Frontend" },
+  { name: "React", hours: 50, category: "Frontend" },
+  { name: "Next.js", hours: 50, category: "Frontend" },
+  { name: "Vue.js", hours: 20, category: "Frontend" },
+  { name: "Nuxt.js", hours: 20, category: "Frontend" },
+
+  // Backend
+  { name: "Node.js", hours: 30, category: "Backend" },
+  { name: "Python", hours: 20, category: "Backend" },
+  { name: "Ruby", hours: 20, category: "Backend" },
+  { name: "Perl", hours: 5, category: "Backend" },
+  { name: "PHP", hours: 50, category: "Backend" },
+  { name: "Java", hours: 20, category: "Backend" },
+  { name: "Kotlin", hours: 25, category: "Backend" },
+  { name: "C#", hours: 100, category: "Backend" },
+  { name: "C", hours: 5, category: "Backend"},
+  { name: "C++", hours: 5, category: "Backend" },
+  { name: "Go", hours: 25, category: "Backend" },
+  { name: "Rust", hours: 15, category: "Backend" },
+  { name: "Dart", hours: 5, category: "Backend" },
+  { name: "Elixir", hours: 5, category: "Backend" },
+  { name: "Erlang", hours: 5, category: "Backend" },
+  { name: "Scala", hours: 5, category: "Backend" },
+  { name: "Swift", hours: 5, category: "Backend" },
+
+  // Database
+  { name: "MySQL", hours: 50, category: "Database" },
+  { name: "PostgreSQL", hours: 20, category: "Database" },
+  { name: "MongoDB", hours: 5, category: "Database" },
+
+  // OS
+  { name: "Linux", hours: 20, category: "OS" },
+  { name: "Windows", hours: 100, category: "OS" },
+
+  // Cloud
+  { name: "AWS", hours: 5, category: "Cloud" },
+  { name: "GCP", hours: 5, category: "Cloud" },
+];
+
+const _skill_frontend = _skill.filter((s) => s.category === "Frontend");
+const _skill_backend = _skill.filter((s) => s.category === "Backend");
+const _skill_database = _skill.filter((s) => s.category === "Database");
+const _skill_os = _skill.filter((s) => s.category === "OS");
+const _skill_cloud = _skill.filter((s) => s.category === "Cloud");
+
+const color = '#0E6DFE';
+
+const skill_frontend: ChartData = {
+  labels: _skill_frontend.map((s) => s.name),
+  datasets: [
+    {
+      label: "Frontend",
+      data: _skill_frontend.map((s) => s.hours),
+      borderWidth: 1,
+      borderColor: color,
+      backgroundColor: color,
+    },
+  ],
+};
+
+const skill_backend: ChartData = {
+  labels: _skill_backend.map((s) => s.name),
+  datasets: [
+    {
+      label: "Backend",
+      data: _skill_backend.map((s) => s.hours),
+      borderWidth: 1,
+      borderColor: color,
+      backgroundColor: color,
+    },
+  ],
+};
+
+const skill_database: ChartData = {
+  labels: _skill_database.map((s) => s.name),
+  datasets: [
+    {
+      label: "Database",
+      data: _skill_database.map((s) => s.hours),
+      borderWidth: 1,
+      borderColor: color,
+      backgroundColor: color,
+    },
+  ],
+};
+
+const skill_os: ChartData = {
+  labels: _skill_os.map((s) => s.name),
+  datasets: [
+    {
+      label: "OS",
+      data: _skill_os.map((s) => s.hours),
+      borderWidth: 1,
+      borderColor: color,
+      backgroundColor: color,
+    },
+  ],
+};
+
+const skill_cloud: ChartData = {
+  labels: _skill_cloud.map((s) => s.name),
+  datasets: [
+    {
+      label: "Cloud",
+      data: _skill_cloud.map((s) => s.hours),
+      borderWidth: 1,
+      borderColor: color,
+      backgroundColor: color,
+    },
+  ],
+};
+
+export { skill_frontend, skill_backend, skill_database, skill_os, skill_cloud };
--- portfolio/pages/skill.tsx
+++ portfolio/pages/skill.tsx
@@ -1,9 +1,34 @@
+import {
+  Chart as ChartJS,
+  registerables,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2';
+import { defaults } from 'chart.js';
 import Layout from '../components/Layout';
+import { skill_frontend, skill_backend, skill_database, skill_os, skill_cloud } from '../data/skill';
+
+ChartJS.register(
+  ...registerables,
+);
+defaults.font.family = 'Consolas';
 
 const SkillPage = () => (
   <Layout title="osawa's skills">
     <div id='Skill'>
-      作成中。
+      <div className='skillUnit'>
+        <h2>Frontend</h2>
+        <Bar
+          className='chart'
+          data={skill_frontend}
+        />
+      </div>
+      <div className='skillUnit'>
+        <h2>Backend</h2>
+        <Bar
+          className='chart'
+          data={skill_backend}
+        />
+      </div>
     </div>
   </Layout>
 )
