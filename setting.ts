b86cae99d57b1f96fc6bd99d0e1ce6d3a7dc3f05
osawa-koki <github@osawa.dev>
2023-07-29 23:47:09 +0900

    履歴書作成した。

--- setting.ts
+++ setting.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import Env from './next.config.js'
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -5,7 +6,19 @@ const setting = {
   isProd,
   basePath: Env.basePath,
   apiPath: isProd ? '' : 'http://localhost:8000',
-  title: '🐙🐬🐸 おおさわ 🐸🐬🐙'
+  title: '🐙🐬🐸 おおさわ 🐸🐬🐙',
+  profile: {
+    name: '大澤 公貴',
+    birthday: dayjs('1998-10-25'),
+    sex: '男性',
+    address: '北海道札幌市',
+    email: 'koki@osawa.cloud',
+    phone: '090-8198-6196',
+    github: {
+      name: 'osawa-koki',
+      uri: 'https://github.com/osawa-koki'
+    }
+  }
 }
 
 export default setting
