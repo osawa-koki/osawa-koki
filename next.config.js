abab8364c4b3e9d3991f803da2cc4bb751a1f5e1
osawa-koki <github@osawa.dev>
2023-07-30 20:29:07 +0900

    サブディレクトリを環境変数で指定するように。

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = '/osawa-koki'
+const SUB_DIRECTORY = process.env.SUB_DIRECTORY ?? ''
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === 'production'
