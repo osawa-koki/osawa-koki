37c5c65af4215595e18bed54f87d59575e844e86
osawa-koki <koki@osawa.dev>
2023-10-25 18:06:01 +0900

    タイプチェックエラーの修正。

--- data/workHostories/02.ts
+++ data/workHostories/02.ts
@@ -10,7 +10,6 @@ export const workHistoryData01: IWork = {
     {
       id: '01',
       title: '新規格対応のための機能追加',
-      description: 'ISMS新規格対応のための機能追加を担当。\n\n実装を主に担当し、設計・テスト・運用保守も一部担当。',
       startDate: dayjs('2023-04'),
       endDate: dayjs('2023-06'),
       technologies: [
@@ -27,7 +26,6 @@ export const workHistoryData01: IWork = {
     {
       id: '02',
       title: '既存システムの不具合対応',
-      description: '既存システムの不具合対応を担当。\n\n原因追及と修正コードの提案を行い、リリース時期の調整とリリースを実施。',
       startDate: dayjs('2023-04'),
       endDate: null,
       technologies: [
