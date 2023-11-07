e530bd8a34617150fcc124f40ffe33bb70407d64
osawa-koki <koki@osawa.dev>
2023-11-07 23:29:39 +0900

    `img` -> `Image`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import setting from '../setting'
 
 export default function Home (): React.JSX.Element {
@@ -6,7 +7,7 @@ export default function Home (): React.JSX.Element {
     <>
       <div id='Index' className='d-flex flex-column align-items-center'>
         <h1>{setting.title}</h1>
-        <img id='Logo' className='mt-3' style={{ width: '100%', maxWidth: '100px' }} src={`${setting.basePath}/tako.png`} alt='Logo' />
+        <Image id='Logo' className='mt-3 mw-100 border rounded-circle' width={100} height={100} src={`${setting.basePath}/tako.png`} alt='Logo' />
       </div>
     </>
   )
