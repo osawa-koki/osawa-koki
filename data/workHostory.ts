93107ff25bb7b801c25bc247c807e9e1aa6a44f6
osawa-koki <koki@osawa.dev>
2024-01-02 19:00:33 +0900

    Notionへ移行。

--- data/workHostory.ts
+++
@@ -1,7 +0,0 @@
-import { workHistoryData00 } from './workHostories/01'
-import { workHistoryData01 } from './workHostories/02'
-
-export const workHostoryData: IWorks = [
-  workHistoryData00,
-  workHistoryData01
-]
